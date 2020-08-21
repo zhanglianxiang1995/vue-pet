523e1326602721dbc7969ecdd295d88f43f74fa3
zhanglianxiang <15665400752@163.com>
2020-08-21 17:23:27 +0800

    登录页面

---
+++ src/components/Login.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="login_container">
+    <img src="../assets/bj.jpg" width="100%" height="100%" alt="">
+    <div class="login_box">
+      <!--头像区域-->
+      <div class="avatar_box">
+        <img src="../assets/petlog.jpg" alt="">
+      </div>
+      <el-form :model="loginfrom" :rules="loginFromRules" label-width="0px" class="login_from">
+        <!-- 用户名-->
+        <el-form-item>
+          <el-input prefix-icon="iconfont icon-yonghu" v-model="loginfrom.username"></el-input>
+        </el-form-item>
+        <!--密码-->
+        <el-form-item>
+          <el-input prefix-icon="iconfont icon-mima" type="password" v-model="loginfrom.password"></el-input>
+        </el-form-item>
+
+        <!--按钮-->
+        <el-form-item class="btns">
+          <el-button type="primary">登录</el-button>
+          <el-button type="info">重置</el-button>
+        </el-form-item>
+
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        //这是登录表单的数据绑定对象
+        loginfrom: {
+          username: '',
+          password: ''
+        },
+        loginFromRules: {
+          username: [
+            {required: true, message: ""}
+          ]
+        }
+      }
+    }
+  }
+</script>
+
+
+<style lang="less" scoped>
+  .login_container {
+    background-color: #2b4b6b;
+    height: 100%;
+  }
+
+  .login_box {
+    width: 450px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 3px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  .avatar_box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+
+  img {
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    background-color: #eee;
+  }
+
+  }
+
+  .login_from {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
+  }
+
+  .btns {
+    display: flex;
+    justify-content: flex-end;
+  }
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import './plugins/element.js'
+import './assets/css/global.css'
+import ElementUIs from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+//字体图标
+import './assets/fonts/iconfont.css'
 
+Vue.config.productionTip = false
+Vue.use(ElementUIs)
 new Vue({
-  el: '#app',
+  router,
   render: h => h(App)
-})
+}).$mount('#app')
