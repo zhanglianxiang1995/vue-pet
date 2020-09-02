dbc4978b8c27011ce227b2dff8faca577a116c53
zhanglianxiang <zhanglianxiang@mac-sky.com>
2020-09-02 21:19:17 +0800

    后台管理页面布局+一级二级菜单实现

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,14 +1,54 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-    登录成功
-  </div>
+  <el-container class="home-container">
+    <!--头部区域-->
+    <el-header>
+      <div>
+        <img src="../assets/syslogo.jpg" alt="宠物商城" width="45px">
+        <span>宠物商城后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!--页面主体-->
+    <el-container>
+      <!--侧边栏目-->
+      <el-aside width="200px">
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#ffd04b">
+          <!--一级菜单-->
+          <el-submenu index="1">
+            <!--一级菜单的模板-->
+            <template slot="title">
+              <!--图标-->
+              <i class="el-icon-location"></i>
+              <!--文本-->
+              <span>导航一</span>
+            </template>
+
+            <el-menu-item index="1-4-1">
+              <template slot="title">
+                <!--图标-->
+                <i class="el-icon-location"></i>
+                <!--文本-->
+                <span>导航一</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!--主体-->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+
 </template>
 
 <script>
   export default {
     methods: {
       logout() {
+        debugger
         window.sessionStorage.clear();
         this.$router.push("/login")
       }
@@ -17,5 +57,36 @@
 </script>
 
 <style lang="less" scoped>
+  .home-container {
+    height: 100%;
+  }
 
+  .el-header {
+    background-color: #373d41;
+    display: flex;
+    justify-content: space-between;
+    padding-left: 15px;
+    align-items: center;
+    color: #fff;
+    font-size: 20px;
+
+  > div {
+    display: flex;
+    align-items: center;
+
+  span {
+    margin-left: 15px;
+  }
+
+  }
+
+  }
+
+  .el-aside {
+    background-color: #333744;
+  }
+
+  .el-main {
+    background-color: #eaedf1;
+  }
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,12 +1,34 @@
 import Vue from 'vue'
-import {Button} from 'element-ui'
-import {Form, FormItem} from 'element-ui'
-import {Input} from "element-ui";
-import {Message} from "element-ui";
+import {
+  Button,
+  Form,
+  FormItem,
+  Input,
+  Message,
+  Container,
+  Header,
+  Aside,
+  Main,
+  Menu,
+  Submenu,
+  MenuItemGroup,
+  MenuItem
+} from 'element-ui'
+
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItemGroup)
+Vue.use(MenuItem)
+
+
 //全局挂载
 Vue.prototype.$message = Message
