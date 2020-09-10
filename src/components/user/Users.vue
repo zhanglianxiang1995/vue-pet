96ea46a80100f44bcf0c5d28d85c3b875323c76a
zhanglianxiang <zhanglianxiang@mac-sky.com>
2020-09-10 21:27:30 +0800

    users

---
+++ src/components/user/Users.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{path:'/home'}">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>活动管理</el-breadcrumb-item>
+      <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+      <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!--卡片视图区域-->
+    <el-card>
+      <!--搜索与添加区域-->
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <el-input placeholder="请输入内容">
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+        </el-col>
+        <el-col :span="4">
+          <el-button type="primary">添加用户</el-button>
+        </el-col>
+      </el-row>
+    </el-card>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        //获取用户列表的参数
+        queryInfo: {
+          query: '',
+          pagenum: 1,
+          pagesize: 2
+        },
+        userlist: [],
+        total: 0
+      }
+    },
+    crated() {
+      this.getUserList()
+    }
+    ,
+    methods: {
+      async getUserList() {
+        await this.$http.get("users", {
+          params: this.queryInfo
+        })
+        if (res.meta.status !== 200) {
+          this.userlist = res.data.users
+          this.total = res.data.total
+          console.log(res)
+        }
+      }
+    }
+  }
+</script>
+<style lang="less" scoped>
+</style>
