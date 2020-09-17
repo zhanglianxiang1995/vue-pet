2b11a3ec61b995278a0cd79370e1a957b4d1efe8
zhanglianxiang <15665400752@163.com>
2020-09-17 17:28:59 +0800

    页面不能跳转

--- src/main.js
+++ src/main.js
@@ -7,13 +7,14 @@ import ElementUIs from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 //字体图标
 import './assets/fonts/iconfont.css'
+import 'babel-polyfill'
 
 //导入axios
 import axios from 'axios'
 
 axios.defaults.baseURL = "http://127.0.0.1:8888/api/private/v1/"
 axios.interceptors.request.use(config => {
-  config.headers.Authorization =window.sessionStorage.getItem("token")
+  config.headers.Authorization = window.sessionStorage.getItem("token")
   return config
 })
 Vue.prototype.$http = axios
--- src/router.js
+++ src/router.js
@@ -4,26 +4,29 @@ import Login from './components/Login.vue'
 import Home from './components/Home.vue'
 import Welcome from './components/Welcome.vue'
 import Users from './components/user/Users.vue'
+import Rights from './components/power/Rights.vue'
+
 Vue.use(Router)
 
 const router = new Router({
   routes: [
     {path: '/', redirect: '/login'},
+    {path: '/login', component: Login},
     {
-      path: '/login', component: Login
-    },
-    {
-      path: '/home', component: Home,
-      redirect:'/users',
+      path: '/home',
+      component: Home,
+      redirect: '/welcome',
       children: [
-        //{path: '/welcome', component: Welcome},
-        {path:'/users',component:Users}
+        {path: '/welcome', component: Welcome},
+        {path: '/users', component: Users},
+        {path: '/rights', component: Rights}
       ]
     },
   ]
 })
 
 //挂载路由导航守卫
+
 router.beforeEach((to, from, next) => {
   //to 将要访问的路径
   //from代表从哪个路径跳转而来
